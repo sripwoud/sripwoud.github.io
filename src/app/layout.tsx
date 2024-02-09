74f98167bec9925936f333ec332c6b30185ed56c
sripwoud <me@sripwoud.xyz>
2024-02-09 13:28:23 +0100

    Update favicon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content='sripwoud' />
+        <link rel='icon' href='/favicon.ico' />
+        <title>sripwoud</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
