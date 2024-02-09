41d75851d46c6d6907b80942ad83f7f85a5759f7
sripwoud <me@sripwoud.xyz>
2024-02-09 13:10:07 +0100

    feat: add chatbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Bubble } from '@typebot.io/nextjs'
 import { Header, Terminal } from '@components'
 
 export default function Home() {
@@ -6,6 +7,16 @@ export default function Home() {
     <>
       <Header />
       <Terminal />
+      <Bubble
+        typebot='sripwoud-contact-form'
+        theme={{
+          button: {
+            backgroundColor: '#120548',
+            customIconSrc:
+              'https://s3.typebot.io/public/workspaces/clsed94k70007c5hfek5smg2e/typebots/clsegah0400048is6i4mww1i2/bubble-icon?v=1707480572075',
+          },
+        }}
+      />
     </>
   )
 }
