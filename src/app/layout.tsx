883dc639ad8e0235545a30850ad7d340119617c4
sripwoud <me@sripwoud.xyz>
2024-10-18 10:46:42 +0200

    style: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='description' content='sripwoud' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/icon.png' type='image/png' sizes="32x32" />
         <title>sripwoud</title>
       </head>
       <body>{children}</body>
